1cadaeda71ecbfc46c506bc4352412475589bdf8
Ahmed Mohamed <ahmedmohamed.amin@hotmail.com>
2024-07-21 07:38:24 +0300

    First commit to initialize a github repo for it

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex bg-blue-950 justify-center items-center h-36 bg text-white">
+      <span className="text-lg">Serga &#x2122; Upskilling Task</span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/sections/Hero/Hero.tsx
@@ -0,0 +1,38 @@
+import HeroLogo from '../../../assets/images/truck-logo.png';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero flex flex-col items-center w-full">
+      <header className="flex gap-4 justify-center pt-16 pb-16">
+        <div className="logo w-18 pt-1">
+          <img
+            src={HeroLogo}
+            alt="recycle truck logo"
+            className="logo-image"
+          />
+        </div>
+        <div className="logo-text flex flex-col">
+          <h1 className="text-5xl tracking-wide">Round Rock</h1>
+          <p className="font-bold text-lg">Trush Hauling</p>
+        </div>
+      </header>
+      <div className="main-hero flex flex-col items-center justify-between py-2">
+        <button className="experts-btn text-white py-2 px-10 rounded-2xl mb-4 text-lg">
+          Experts
+        </button>
+        <h2 className="text-8xl lorem-header py-3 font-thin">
+          Lorem
+        </h2>
+        <h2 className="text-8xl font-extrabold">Lorem lorem</h2>
+        <p className="text-center max-w-lg pt-5 pb-10 font-semibold text-lg">
+          Lorem Ipsum is simply dummy text of the printing and
+          typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text ever since the
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
